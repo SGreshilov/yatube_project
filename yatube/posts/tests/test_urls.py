a8e81fd7cf56062bcb8889ef9053f6a1df61d7f3
Стас Грешилов <kartliss0@mail.ru>
2025-02-19 16:50:41 +0300

    1111

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -12,33 +12,55 @@ class PostURLTests(TestCase):
     def setUpClass(cls):
         """Добавление тестовых записей в БЖ"""
         super().setUpClass()
-        user = User.objects.create_user(username='auth')
+        cls.user = User.objects.create_user(username='test')
         Group.objects.create(
             title='тестовое название',
             slug='test-test',
             description='тестовое описание'
         )
-        Post.objects.create(
-            text='Тестовый текст',
-            author=user
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый текст'
         )
+        cls.post_id = cls.post.pk
 
     def setUp(self):
         """Создание авторизованного и неавторизованного пользователей"""
         self.guest_client = Client()
-        self.user = User.objects.get(username='auth')
         self.authorized_client = Client()
-        self.authorized_client.force_login(self.user)
+        self.authorized_client.force_login(PostURLTests.user)
 
-    def test_code_post_pages(self):
-        """Страницы /group/test-test/, '/', /profile/auth/, /posts/1/ доступны всем"""
+    def test_urls_uses_correct_template(self):
+        """URL-адреса используют соответствующий шаблон"""
+        post_id = PostURLTests.post_id
         templates_url_names = {
             '/group/test-test/': 'posts/group_list.html',
-            '/profile/auth/': 'group/profile.html',
-            '/posts/2/': 'posts/post_detail.html',
+            '/profile/test/': 'posts/profile.html',
+            f'/posts/{post_id}/': 'posts/post_detail.html',
             '/': 'posts/index.html',
+            '/create/': 'posts/create_post.html',
+            f'/posts/{post_id}/edit/': 'posts/create_post.html'
         }
         for address, template in templates_url_names.items():
             with self.subTest(address=address):
-                response = self.guest_client.get(address)
-                self.assertEqual(response.status_code, 200, address)
+                response = self.authorized_client.get(address)
+                self.assertTemplateUsed(response, template)
+
+    def test_post_create_redirect_anon(self):
+        """Проверка доступности страницы создания поста неавторизованному пользователю"""
+        response = self.guest_client.get('/create/')
+        self.assertRedirects(response, '/auth/login/?next=/create/')
+
+    def test_post_edit_redirect_no_author(self):
+        """Проверка доступности страницы редактирования поста не для автора"""
+        post_id = PostURLTests.post_id
+        user = User.objects.create_user(username='noauthor')
+        client = Client()
+        client.force_login(user)
+        response = client.get(f'/posts/{post_id}/edit/')
+        self.assertRedirects(response, f'/posts/{post_id}/')
+
+    def test_unexisting_page(self):
+        """Проверка на получение ошибки 404 при запросе на несуществующую страницу"""
+        response = self.client.get('poz/asdz/')
+        self.assertEqual(response.status_code, 404)
